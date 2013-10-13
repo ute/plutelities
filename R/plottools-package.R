a084b445ac495d06f0e1cb89d279d446671b7351
ute <utelity@gmail.com>
2013-10-13 11:48:40 +0200

    0.1 Description of intended contents

---
+++ R/plottools-package.R
@@ -0,0 +1,31 @@
+#'@name plottools-package
+#'@aliases plottools-package, plottools
+#'@docType package
+#'@title Plotting with Style
+#'@description 
+#'  Provides utilities for handling plot and graphic parameters, 
+#'  plotting lists of objects, colour tools. 
+#'  Plotting with style: plot parameters can be given as lists.
+#'@details  
+#\tabular{ll}{
+#      Package: \tab plottools
+#  \cr Type: \tab Package
+#  \cr Version: \tab 0.1
+#  \cr Date: \tab 2013-10-13
+#  \cr License: \tab GNUv2
+#  }
+#' 
+#' \strong{Functions for plotting with style, and plotting lists}  
+#' \tabular{ll}{
+#'  \code{\link{splot}} \tab plot using a list of parameters (a "style")
+#'  \cr\code{\link{lplot}} \tab plot all objects in a list
+#'  } 
+#' \strong{Utility functions}  
+#' \tabular{ll}{
+#'  \code{\link{lightcol}} \tab produce a lighter version of a color
+#'  \cr\code{\link{dissectparlist}} \tab sorts a list of parameters 
+#'  into graphic, plot and other parameters
+#'  } 
+#'@author Ute Hahn, \email{ute@@imf.au.dk}
+# @examples
+NA
