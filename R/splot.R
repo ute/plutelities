b453ad0d88e15f16005eb008f8deee4e3f84077d
utelity <utelity@gmail.com>
2013-10-15 21:51:11 +0200

    0.2.1 little bug fix when using 'builtin' plot methods: .plotmethod should always be given as character to avoid errors

--- R/splot.R
+++ R/splot.R
@@ -5,7 +5,7 @@
 #'@param x an object that belongs to a class with \code{plot} method.
 #'@param ... plot parameters, may be wrapped in \code{\link{style}} lists.
 #'@param NULL.rm logical. If \code{TRUE}, ignore parameters with value \code{NULL}.
-#'@param .plotmethod function, dafaults to \code{plot}.
+#'@param .plotmethod character naming a function, defaults to \code{"plot"}.
 #'@details The function calls the \code{plot}-method of \code{x}, with arguments
 #' given in \code{...}. Any \code{\link{style}}s contained in the arguments are
 #' unwrapped. If arguments with the same name appear in the list, the last one
@@ -23,7 +23,7 @@
 #'splot(sin, boldly, timidly)
 #'splot(sin, timidly, boldly, col = "green")
 
-splot <- function(x, ..., NULL.rm = TRUE, .plotmethod = plot) {
+splot <- function(x, ..., NULL.rm = TRUE, .plotmethod = "plot") {
   plotargs <- style(..., NULL.rm = NULL.rm)
   do.call(.plotmethod, c(list(x), plotargs))
 }
