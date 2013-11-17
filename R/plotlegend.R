2d3a19dd93155a2575eb86c4750cfaa477b9d679
Ute <utelity@gmail.com>
2013-11-17 12:26:17 +0100

    updated documentation and added example for plotlegend

--- R/plotlegend.R
+++ R/plotlegend.R
@@ -5,10 +5,17 @@
 #'given as simplists.
 #'@export
 #'@param ... arguments specifying the graphic legend,  
-#'@details Generic function for plotting legends. Intended for use with objects
+#'@details Generic function for plotting legends, intended for use with objects
 #'that carry legend information. The default method is a wrapper for the base
-#'function \code{\link{legend}}
+#'function \code{\link{legend}}, and allows to use \code{\link{simplist}}s.
 #'@seealso \code{\link{legend}} for the usual parameters.
+#'@examples
+#'# plot some curves
+#'plot(cos, col = "red", to = pi, ylab = "y")
+#'plot(sin, col = "green", to = pi, add = TRUE)
+#'# use of simplists:
+#'legendstuff <- simplist(legend = c("y = sin(x)", "y = cos(x)"), col = c("green", "red"))
+#'plotlegend("bottomleft", legendstuff, lty = "solid")
 
 plotlegend <- function(...) UseMethod("plotlegend")
 
@@ -17,5 +24,5 @@ plotlegend <- function(...) UseMethod("plotlegend")
 #
 plotlegend.default <- function (...)
 {
-  do.call(splot, c(list(...), .plotmethod = "legend"))
+  do.call(splot, c(simplist(...), .plotmethod = "legend"))
 }
